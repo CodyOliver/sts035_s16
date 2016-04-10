91923afefdfe4ced94f12f6c175442992e4be88a
DEYO <mdeyo@mit.edu>
2016-04-10 15:14:16 -0400

    worked on convert_data script to take in other csv paths

--- Python/convert_data.py
+++ Python/convert_data.py
@@ -1,5 +1,20 @@
 import pandas
+import sys
 
-data = pandas.read_csv('/Users/Chengzhendai/Downloads/Content Data Sets.csv')
+'''
+Call this convert_data from the command line with the second argument of csv name
+for example:
 
-data.info()
+	python convert_data.py test.csv
+
+'''
+
+inputPath = sys.argv[1]
+
+print inputPath
+
+#data = pandas.read_csv('/Users/Chengzhendai/Downloads/Content Data Sets.csv')
+
+data = pandas.read_csv(inputPath)
+#data.info()
+print data 

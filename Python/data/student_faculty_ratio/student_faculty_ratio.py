bd55a929fd1068b200c29e4ded08eff370243f12
Chengzhen Dai <chengdai@mit.edu>
2016-04-26 22:41:23 -0400

    added more data

--- Python/data/student_faculty_ratio/student_faculty_ratio.py
+++ Python/data/student_faculty_ratio/student_faculty_ratio.py
@@ -1,6 +1,8 @@
 import pandas
 import matplotlib
 import matplotlib.pyplot as plt
+import ast
+import numpy
 
 #Set the style of the graph
 matplotlib.style.use('ggplot') 
@@ -12,14 +14,25 @@ data = pandas.read_csv('student_faculty_ratio.csv')
 data2 = data.iloc[::3,:]
 data2 = data2.set_index('Year') 
 
-data2.to_csv('student_to_faculty_ratio.csv')
+rgb = ['(0,0,255)']*len(data2.index)
+data2['RGB'] = rgb
+data2.loc[data2.index == 1943, 'RGB'] = '(255,0,0)'
+data2.loc[data2.index == 1946, 'RGB'] = '(255,0,0)'
 
+rgb_list = list(data2['RGB'].values)
+for i in range(len(rgb_list)):
+	rgb_list[i] = ast.literal_eval(rgb_list[i])
 
+print list(data2.index), list(300-3*numpy.array(data2['Student-Faculty Ratio'].values)), rgb_list
+
+#data2.to_csv('student_to_faculty_ratio.csv')
+
+'''
 #Plot the graph by setting the x axis to the 'Year' column and y axes to 'Student-Faculty Ratio'
 plt.figure()
 data2.plot(x = data2.index, y = 'Student-Faculty Ratio') 
 plt.show()
-
+'''
 
 #Extract last 100 years (rows_ from the original data set
 data3 = data.iloc[-100:,:] 
@@ -27,9 +40,9 @@ data3 = data.iloc[-100:,:]
 #Extract every other row from last 100 years
 data3 = data3.iloc[::2,:]
 
-
+'''
 #Plot the graph by setting the x axis to the 'Year' column and y axes to 'Student-Faculty Ratio'
 plt.figure()
 data3.plot(x = 'Year', y = 'Student-Faculty Ratio') 
 plt.show()
-
+'''
